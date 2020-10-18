2cc68d7d4e682aa6acb92d99be1342b8e0a184ef
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-18 13:22:28 +0200

    update App class to include method for adding new Restaurants

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 class App {
-    constructor(map) {
-    }
-    setMap(map){
-        this.map = map
-    }
+	constructor() {
+		/** @type {[Restaurant]} */
+		this.resturants = [];
+		this.newRestaurantName = '';
+		this.newRestaurantAddress = '';
+		this.newRestaurantPosition = {};
+	}
+	setMap(map) {
+		this.map = map;
+		this.addMapEvents();
+	}
+
+	addMapEvents() {
+		this.map.addListener('rightclick', (e) => {
+			this.newRestaurantPosition = e.latLng;
+			this.showModal();
+		});
+	}
+
+	showModal() {
+		document.querySelector('.modal-overlay').style.display = 'block';
+	}
+
+	createNewRestaurant(restaurantName, restaurantAddress) {
+		this.newRestaurantName = restaurantName;
+		this.newRestaurantAddress = restaurantAddress;
+		this.addNewRestaurant();
+	}
+
+	addNewRestaurant() {
+		const restPosition = {
+			lat: this.newRestaurantPosition.lat(),
+			lng: this.newRestaurantPosition.lng(),
+		};
+		const { newRestaurantName, newRestaurantAddress } = this;
+		const restaurant = new Restaurant(
+			newRestaurantName,
+			newRestaurantAddress,
+			restPosition,
+		);
+		this.resturants.push(restaurant);
+		const marker = new google.maps.Marker({
+			map: this.map,
+			position: restPosition,
+		});
+	}
 }
