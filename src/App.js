acfd7dd7c3ba2b4b6e494fdfc6a43c64fb95a3e0
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-05 11:14:51 +0200

    update the main App file to include filterd resturants

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 class App {
 	constructor() {
 		/** @type {[Restaurant]} */
-		this.resturants = [];
+		this.mapRestaurants = [];
+		this.markers = [] ;
+		this.userMarkers = [] ;
+		this.userRestaurants = [] ;
+		this.currrentLocation = '';
 		this.newRestaurantName = '';
 		this.newRestaurantAddress = '';
 		this.newRestaurantPosition = {};
+		this.filteredRestaurants = [] ;
+		this.minStars = 1 ;
+		this.maxStars= 5
 	}
-	setMap(map) {
+	setMap(map,position) {
 		this.map = map;
+		this.currrentLocation = position ;
+		this.places =  new google.maps.places.PlacesService(map);
+		let request = {
+			location: this.currrentLocation,
+			radius: 500,
+			types: ['restaurant']
+		}
+		this.places.nearbySearch(request,(result,status)=>{
+			if(status === 'OK') {
+					this.findPlaces();
+			}
+		})
 		this.addMapEvents();
 	}
 
@@ -16,6 +35,9 @@ class App {
 			this.newRestaurantPosition = e.latLng;
 			this.showModal();
 		});
+		this.map.addListener('dragend',()=>{
+			this.findPlaces()
+		})
 	}
 
 	showModal() {
@@ -39,10 +61,55 @@ class App {
 			newRestaurantAddress,
 			restPosition,
 		);
-		this.resturants.push(restaurant);
+		this.userRestaurants.push(restaurant);
 		const marker = new google.maps.Marker({
 			map: this.map,
 			position: restPosition,
+			id:restaurant.id,
+			placesId:restaurant.id
 		});
+		this.userMarkers.push(marker) ;
+	}
+	findPlaces(){
+		this.clearRestaurants();
+		this.clearMarkers();
+		const bounds = {
+			bounds: this.map.getBounds(),
+			types: ['restaurant']
+		}
+			this.places.nearbySearch(bounds, (results,status)=> {
+				console.log(status)
+				if(status === "OK"){
+					results.forEach(restaurant=>{
+						console.log(restaurant);
+						this.mapRestaurants.push(restaurant) ;
+						this.makeRestaurantMarker(restaurant)
+					})
+					this.filterRestaurants();
+				}
+
+			})
+		}
+
+	makeRestaurantMarker(restaurant) {
+		const marker = new google.maps.Marker({
+			map:this.map,
+			placeId:restaurant.id,
+			id:restaurant.id,
+			position:restaurant.geometry.location,
+			zIndex:5
+		})
+		this.markers.push(marker);
+	}
+	clearRestaurants(){
+		this.mapRestaurants = [] ;
+	}
+	clearMarkers(){
+		this.markers.forEach(mark=>mark.setMap(null));
+		this.markers = [] ;
+	}
+	filterRestaurants(){
+		this.filteredRestaurants = this.mapRestaurants.filter(rest=>rest.rating >= this.minStars && rest.rating <=this.maxStars);
+		UI.displayRestaurant(this.filteredRestaurants) ;
 	}
 }
